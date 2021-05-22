151dab8d8610abb2e17365e986ccf1ac8be34c9f
Rakesh Vadde <51173456+vadderakesh@users.noreply.github.com>
2021-05-22 13:09:11 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function get() {
+const Http = new XMLHttpRequest();
+const url='https://clist.by/api/v1/json/contest/?resource__id=2&start_gt=2021-05-20T03:07:43&username=rakhi&api_key=c8cb35a0a9795a719a92602f4578ea9e25d8592c';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  var json=Http.responseText;
+  var object=JSON.parse(json);
+  var totalContests=object.meta.total_count;
+ // console.log(object);
+  var objectsArray=object.objects;
+  for(var i=0;i<100;i++) {
+    var id=objectsArray[i].id;
+    var 
+    console.log(id);
+  }
+  document.getElementById("op").innerHTML=totalContests;
+  };
+  }
